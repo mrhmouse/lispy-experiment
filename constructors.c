75e2c8927562d50c3f89e7afe3efb5b1662f8887
Jordan Brown <mrhmouse@gmail.com>
2015-03-10 19:01:44 +0000

    Use pointers instead, so that freeing is consistent

--- constructors.c
+++ constructors.c
@@ -1,31 +1,60 @@
 #include "constructors.h"
+#include <stdlib.h>
 #include <string.h>
+#define NEW(name) name = malloc(sizeof(*name))
 
-struct cell int_cell(int value) {
-  struct leaf l = { L_INTEGER, value };
-  struct cell c = { C_LEAF, l };
+static struct cell *leaf(struct leaf l) {
+  struct cell *c;
+  NEW(c);
+  c->type = C_LEAF;
+  c->data.leaf = l;
   return c;
 }
 
-struct cell double_cell(double value) {
-  struct leaf l = { L_FLOATING, value };
-  struct cell c = { C_LEAF, l };
+struct cell *int_cell(int value) {
+  struct leaf l = { L_INTEGER, .data.integer = value };
+  return leaf(l);
+}
+
+struct cell *double_cell(double value) {
+  struct leaf l = { L_FLOATING, .data.floating = value };
+  return leaf(l);
+}
+
+struct cell *symbol_cell(char *value) {
+  struct leaf l = { L_SYMBOL, .data.symbol = strdup(value) };
+  return leaf(l);
+}
+
+struct cell *nil() {
+  struct cell *c;
+  NEW(c);
+  c->type = C_NIL;
   return c;
 }
 
-struct cell symbol_cell(char *value) {
-  struct leaf l = { L_SYMBOL, strdup(value) };
-  struct cell c = { C_LEAF, l };
-  return c;
-}
-
-struct cell nil() {
-  struct cell c = { C_NIL, NULL };
-  return c;
-}
-
-struct cell cons(struct cell *head, struct cell *tail) {
+struct cell *cons(struct cell *head, struct cell *tail) {
   struct pair p = { head, tail };
-  struct cell c = { C_PAIR, p };
+  struct cell *c;
+  NEW(c);
+  c->type = C_PAIR;
+  c->data.pair = p;
   return c;
+}
+
+void free_cell(struct cell *c) {
+  switch (c->type) {
+    case C_NIL: return;
+    case C_PAIR:
+      free_cell(c->data.pair.head);
+      free_cell(c->data.pair.tail);
+      free(c->data.pair.head);
+      free(c->data.pair.tail);
+      break;
+    case C_LEAF:
+      if (c->data.leaf.type == L_SYMBOL) {
+        free(c->data.leaf.data.symbol);
+      }
+      break;
+  }
 }
--- constructors.h
+++ constructors.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "data_types.h"
 
-struct cell int_cell(int value);
-struct cell double_cell(double value);
-struct cell symbol_cell(char *value);
-struct cell nil();
-struct cell cons(struct cell *, struct cell *);
+struct cell *int_cell(int value);
+struct cell *double_cell(double value);
+struct cell *symbol_cell(char *value);
+struct cell *nil();
+struct cell *cons(struct cell *, struct cell *);
+
+void free_cell(struct cell *);
