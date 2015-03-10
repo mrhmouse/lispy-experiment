4065833ca2673cea59802b8460036cb74e88e46e
Jordan Brown <mrhmouse@gmail.com>
2015-03-10 19:13:39 +0000

    Add testing program

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "sexpr.h"
+#include <stdio.h>
+
+int main() {
+  struct cell *items =
+    cons(
+      symbol_cell("*"),
+      cons(
+        int_cell(2),
+        cons(
+          cons(
+            symbol_cell("+"),
+            cons(
+              int_cell(3),
+              cons(
+                int_cell(4),
+                nil()))),
+          nil())));
+
+  puts("\n----");
+  print_cell(items);
+  free_cell(items);
+  puts("\n----");
+
+  return 0;
+}
